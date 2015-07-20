0a7e182dbb63ca3c7fb79e92046383f9bced7222
Takumi Akiyama <t.akiym@gmail.com>
2015-07-20 22:59:00 +0900

    Perl-1

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -1,25 +1,42 @@
 package Log;
 use strict;
 use warnings;
+use Time::Piece ();
+use URI;
 
 sub new {
     my ($class, %args) = @_;
-    return bless \%args, $class;
+    my $self = bless \%args, $class;
+    $self->_parse_http_header();
+    return $self;
 }
 
-sub protocol {
-}
+sub protocol { $_[0]->{_protocol} }
 
-sub method {
-}
+sub method { $_[0]->{_method} }
 
-sub path {
-}
+sub path { $_[0]->{_path} }
 
 sub uri {
+    my ($self) = @_;
+    my $uri = URI->new($self->path);
+    $uri->scheme('http');
+    $uri->host($self->{host});
+    return $uri->as_string;
 }
 
 sub time {
+    my ($self) = @_;
+    my $t = Time::Piece->gmtime($self->{epoch});
+    return $t->datetime;
+}
+
+sub _parse_http_header {
+    my ($self) = @_;
+    my ($method, $path, $protocol) = split /\s+/, $self->{req};
+    $self->{_method} = $method;
+    $self->{_path} = $path;
+    $self->{_protocol} = $protocol;
 }
 
 1;
