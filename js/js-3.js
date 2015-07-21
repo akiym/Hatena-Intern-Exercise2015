f7c2a3e93a966802e0b0aa194208216e4dbe8ba4
Takumi Akiyama <t.akiym@gmail.com>
2015-07-21 17:13:56 +0900

    JS-3

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,13 @@
 // 課題 JS-3 の実装をここに記述してください。
+(function() {
+  var submitButton = document.getElementById('submit-button');
+  submitButton.addEventListener('click', function() {
+    var tableContainer = document.getElementById('table-container');
+    while (tableContainer.hasChildNodes()) {
+      tableContainer.removeChild(tableContainer.firstChild);
+    }
+    var logInput = document.getElementById('log-input');
+    var logRecords = parseLTSVLog(logInput.value);
+    createLogTable(tableContainer, logRecords);
+  }, false);
+})();
