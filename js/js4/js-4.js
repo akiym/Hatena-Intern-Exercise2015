3a864889b9b16024bf8ee77ec45ed62cdaf9f4d4
Takumi Akiyama <t.akiym@gmail.com>
2015-08-02 18:37:05 +0900

    not検索できるようにした

--- js/js4/js-4.js
+++ js/js4/js-4.js
@@ -11,17 +11,43 @@
   });
 
   function searchQuery(query) {
-    var re = new RegExp(query, 'i');
+    var queries = $.map(query.split(/\s+/), function(q) { return quoteRegExpMeta(q) });
+    var pattern = [];
+    var notPattern = [];
+    query.split(/\s+/).forEach(function(q) {
+      if (q === '') {
+      } else if (q.length > 1 && q.match(/^-/)) {
+        notPattern.push(quoteRegExpMeta(q.slice(1)));
+      } else {
+        pattern.push(quoteRegExpMeta(q));
+      }
+    });
+    var re = new RegExp(pattern.join('|'), 'i');
+    var reNot = new RegExp(notPattern.join('|'), 'i');
+    var not = notPattern.length > 0;
     var filterdLogRecords = [];
     logRecords.forEach(function(record) {
-      Object.keys(record).forEach(function(key) {
-        if (String(record[key]).match(re)) {
-          filterdLogRecords.push(record);
+      var matched = false;
+      var keys = Object.keys(record);
+      for (var i = 0; i < keys.length; i++) {
+        var value = String(record[keys[i]]);
+        if (not && value.match(reNot)) {
+          matched = false;
+          break;
+        } else if (value.match(re)) {
+          matched = true;
         }
-      });
+      }
+      if (matched) {
+        filterdLogRecords.push(record);
+      }
     });
     var tableContainer = document.getElementById('table-container');
     createLogTable(tableContainer, filterdLogRecords);
+  }
+
+  function quoteRegExpMeta(q) {
+    return q.replace(/([\\^$*+?.()[\]{}|])/g, '\\$1');
   }
 
   //----------
